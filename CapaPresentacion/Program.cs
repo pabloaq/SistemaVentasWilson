e0e1f84d1de78612619f5b6636e71bf7da933457
JordanRondon <jordanangelrondonpozo@gmail.com>
2022-10-31 23:44:46 -0500

    se creo el archivo para el formulario pedido

--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -16,7 +16,7 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new formMetodoPago());
+            Application.Run(new FormPedido());
         }
     }
 }

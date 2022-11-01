b08cd1dc6de9e68aee4e0434be690895315ed23c
BrandokVargas <brandokvargas27@gmail.com>
2022-10-31 20:15:48 -0500

    Agregamos el formulario metodo de pago

---
+++ CapaPresentacion/formMetodoPago.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class formMetodoPago : Form
+    {
+        int x = 0, y = 0;
+        public formMetodoPago()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                x = e.X;
+                y = e.Y;
+            }
+            else
+            {
+                Left = Left + (e.X - x);
+                Top = Top + (e.Y - y);
+                
+
+            }
+        }
+    }
+}

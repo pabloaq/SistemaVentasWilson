fa0c921cd563974fdeb0bea36ef2fb2a2e927b71
PaulRodMurga <aderholdpaul@gmail.com>
2022-11-01 08:24:55 -0500

    Formulario Mantenedor Producto

---
+++ CapaPresentacion/MantenedorProducto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MantenedorProducto
+{
+    public partial class MantenedorProducto : Form
+    {
+        public MantenedorProducto()
+        {
+            InitializeComponent();
+        }
+
+        private void MantenedorProducto_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

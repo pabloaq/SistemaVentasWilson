0a5cf65a48b2c5e26a192831540d478f45d6e4f7
JordanRondon <jordanangelrondonpozo@gmail.com>
2022-11-10 12:24:28 -0500

    se inserto el icono al formulario inicio

--- CapaPresentacion/FormInicio.Designer.cs
+++ CapaPresentacion/FormInicio.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormInicio));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.btnAdmin = new System.Windows.Forms.Button();
@@ -126,6 +127,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(464, 205);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(480, 244);
             this.MinimumSize = new System.Drawing.Size(480, 244);
             this.Name = "FormInicio";

5448c448aa4de74e75bc18c97e50aa2b8d64a19b
pabloaq <alfaroquispepablo@gmail.com>
2022-11-19 20:18:43 -0500

    Se agrego la clase Conexion

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        private static readonly Conexion instancia = new Conexion();
+
+        public static Conexion GetInstancia => instancia;
+
+        public SqlConnection Conectar => new SqlConnection("Data Source=.;Initial Catalog=dbSistemaVentas;" + "Integrated Security=true;");
+
+    }
+}
